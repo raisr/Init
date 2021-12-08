0add8c56bbf01eb69eaccff46aad48c89c856bbb
raisr <info@raisr.net>
2021-12-08 21:04:26 +0100

    Create folder to store settings in home folder

--- scripts/setup.ps1
+++ scripts/setup.ps1
@@ -1,5 +1,14 @@
 Write-Host "Setting up environment"
 
+$targetPath = "$home\.env-config"
+Write-Host "Creating directory for settings / resources: $targetPath"
+
+
+If(!(test-path $targetPath))
+{
+      New-Item -ItemType Directory -Force -Path $targetPath
+}
+
 & "$PSScriptRoot\install-requirements.ps1"
-& "$PSScriptRoot\install-fonts.ps1"
-& "$PSScriptRoot\initialize-shell.ps1"
+# & "$PSScriptRoot\install-fonts.ps1"
+# & "$PSScriptRoot\initialize-shell.ps1"
