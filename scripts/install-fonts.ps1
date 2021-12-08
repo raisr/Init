db8670f0d23893974e416e0c164199c3aaef8414
raisr <info@raisr.net>
2021-12-08 22:17:35 +0100

    Updated font installer script

--- scripts/install-fonts.ps1
+++ scripts/install-fonts.ps1
@@ -14,7 +14,7 @@ $objFolder = $objShell.Namespace($FONTS)
 $fonts = Get-ChildItem $fontPath -Filter "*.ttf" -Recurse
 foreach ($File in $fonts) 
 {
-   if ((Test-Path "C:\Windows\Fonts\$File") -eq $False) 
+   if (!(Test-Path "C:\Windows\Fonts\$File")) 
    {
       $objFolder.CopyHere($File.fullname, 0x10)
    }
